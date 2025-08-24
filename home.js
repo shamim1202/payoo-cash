a9ab5f9964f11352b98e54e5a7f2d47eca32facc
shamim1202 <mehedi.farzu@gmail.com>
2025-08-24 12:20:45 +0600

    All Add Money Functionallity JavaScript Code Validated and modified code

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const pin = 1234;
+
 document.getElementById("btn-add-money").addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -6,12 +8,30 @@ document.getElementById("btn-add-money").addEventListener("click", (e) => {
     const addMoney = parseInt(
         document.getElementById("add-amount-input").value
     );
-    const bankPin = document.getElementById("bank-pin-input").value;
+    const bankPin = parseInt(document.getElementById("bank-pin-input").value);
     const availableBalance = parseInt(
         document.getElementById("available-balance").innerText
     );
+    console.log(typeof addMoney === typeof availableBalance);
 
-    const netBalance = availableBalance + addMoney;
-
-    document.getElementById("available-balance").innerText = netBalance;
+    if (bankName === "") {
+        return alert("A Bank Account Required!!!");
+    }
+    if (bankAccount.length !== 9) {
+        return alert("Invalid Account Number");
+    }
+    if (isNaN(addMoney)) {
+        return alert("Please Enter Your Desire Amount");
+    }
+    if (addMoney < 100 || addMoney > 50000) {
+        return alert("Add Valid Amount Between 100 to 50000");
+    }
+    if (bankPin !== pin) {
+        return alert("Invalid Bank Account Pin");
+    } else {
+        const netBalance = availableBalance + addMoney;
+        document.getElementById("available-balance").innerText = netBalance;
+        alert(`${addMoney} Add Money Successfully Done.`);
+        document.getElementById("add-money-form").reset();
+    }
 });
